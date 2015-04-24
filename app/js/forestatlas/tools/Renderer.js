eb26cf76b47d8c500d64f729945dda77097270b9
Robert Winterbottom <rwinterbottom@blueraster.com>
2015-04-24 17:45:20 -0400

    fix for csv exporting issues

--- app/js/forestatlas/tools/Renderer.js
+++ app/js/forestatlas/tools/Renderer.js
@@ -472,43 +472,104 @@ define([
     	// Generate the desired output
 			// Try the provided methods first as they will allow the file to be named
 			// worst case, use the final option
-			var hrefValue = 'data:application/csv;charset=utf-8,',
-					blobType = 'text/csv;charset=latin;',
+			var hrefValue = 'data:application/vnd.ms-excel;base64,',
+					blobType = 'text/csv;charset=utf-8;',
 					link = document.createElement('a'),
-					data = encodeURI(csvStringData),
 					filename = 'data.csv',
 					blob;
 
+			if (window.navigator.msSaveOrOpenBlob) {
 
-			// If the FileSaver is loaded from cdn correctly and is supported in this browser, use it
-			if (saveAs && !!new Blob) {
-
-				blob = new Blob(["\uFEFF" + csvStringData], {
-					type: blobType
-				});
+				blob = this.base64toBlob(this.base64_encode(csvStringData), blobType);
+				navigator.msSaveBlob(blob, filename);
+				
+			} else if (saveAs && !!new Blob) {
+				// If the FileSaver is loaded from cdn correctly and is supported in this browser, use it
+				blob = this.base64toBlob(this.base64_encode(csvStringData), blobType);
 				saveAs(blob, filename);
 
-			// These options should not be needed as they are probably part of the FileSaver code itself
-			// } else if (link.download !== "") {
+			} else if (link.download === "") {
 
-			// 	link.href = hrefValue + encodeURIComponent(csvStringData);
-			// 	link.target = '_blank';
-			// 	link.download = filename;
-			// 	link.click();
-
-			// } else if (window.navigator.msSaveOrOpenBlob) {
-
-			// 	blob = new Blob([decodeURIComponent(data)], {
-			// 		type: blobType
-			// 	});
-			// 	navigator.msSaveBlob(blob, filename);
+				link.href = hrefValue + this.base64_encode(csvStringData);
+				link.target = '_blank';
+				link.download = filename;
+				link.click();
 
 			} else {
-
-				window.open(hrefValue + data);
-
+				window.open(hrefValue + this.base64_encode(data));
 			}
     },
+
+    base64toBlob: function (base64Data, contentType) {
+    	// Taken From: http://stackoverflow.com/questions/16245767/creating-a-blob-from-a-base64-string-in-javascript
+    	// Only works in 
+		    contentType = contentType || '';
+		    var sliceSize = 1024;
+		    var byteCharacters = atob(base64Data);
+		    var bytesLength = byteCharacters.length;
+		    var slicesCount = Math.ceil(bytesLength / sliceSize);
+		    var byteArrays = new Array(slicesCount);
+
+		    for (var sliceIndex = 0; sliceIndex < slicesCount; ++sliceIndex) {
+		        var begin = sliceIndex * sliceSize;
+		        var end = Math.min(begin + sliceSize, bytesLength);
+
+		        var bytes = new Array(end - begin);
+		        var i = 0;
+		        for (var offset = begin; offset < end; ++i, ++offset) {
+		            bytes[i] = byteCharacters[offset].charCodeAt(0);
+		        }
+		        byteArrays[sliceIndex] = new Uint8Array(bytes);
+		    }
+		    return new Blob(byteArrays, { type: contentType });
+		},
+
+    base64_encode: function  (data) {
+		  //  discuss at: http://phpjs.org/functions/base64_encode/
+		  // original by: Tyler Akins (http://rumkin.com)
+		  // improved by: Bayron Guevara
+		  // improved by: Thunder.m
+		  // improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+		  // improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+		  // improved by: Rafał Kukawski (http://kukawski.pl)
+		  // bugfixed by: Pellentesque Malesuada
+		  //   example 1: base64_encode('Kevin van Zonneveld');
+		  //   returns 1: 'S2V2aW4gdmFuIFpvbm5ldmVsZA=='
+		  //   example 2: base64_encode('a');
+		  //   returns 2: 'YQ=='
+
+		  var b64 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+		  var o1, o2, o3, h1, h2, h3, h4, bits, i = 0,
+		    ac = 0,
+		    enc = '',
+		    tmp_arr = [];
+
+		  if (!data) {
+		    return data;
+		  }
+
+		  do { // pack three octets into four hexets
+		    o1 = data.charCodeAt(i++);
+		    o2 = data.charCodeAt(i++);
+		    o3 = data.charCodeAt(i++);
+
+		    bits = o1 << 16 | o2 << 8 | o3;
+
+		    h1 = bits >> 18 & 0x3f;
+		    h2 = bits >> 12 & 0x3f;
+		    h3 = bits >> 6 & 0x3f;
+		    h4 = bits & 0x3f;
+
+		    // use hexets to index into b64, and append result to encoded string
+		    tmp_arr[ac++] = b64.charAt(h1) + b64.charAt(h2) + b64.charAt(h3) + b64.charAt(h4);
+		  } while (i < data.length);
+
+		  enc = tmp_arr.join('');
+
+		  var r = data.length % 3;
+
+		  return (r ? enc.slice(0, r - 3) : enc) + '==='.slice(r || 3);
+		},
 
     /**
     * Takes a title like "MyFeature (1 of 3)" and returns "MyFeature"
