77a7c474e8bae6f3fbb2091abdff99cf0a586d6e
Robert Winterbottom <rwinterbottom@blueraster.com>
2015-06-11 14:03:05 -0400

    added another year to the slider for forest loss

--- app/js/forestatlas/tools/ToolsModel.js
+++ app/js/forestatlas/tools/ToolsModel.js
@@ -83,7 +83,8 @@ define(
                         // Items for Year Dropdown for forest cover loss layer
                         o._vm.forestLossYears = ko.observableArray([
                             '2001', '2002', '2003', '2004', '2005', '2006',
-                            '2007', '2008', '2009', '2010', '2011', '2012'
+                            '2007', '2008', '2009', '2010', '2011', '2012',
+                            '2013'
                         ]);
 
                         // Items for Analysis
