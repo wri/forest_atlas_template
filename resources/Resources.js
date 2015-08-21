16e87b7bdd0fca6d703c8100c13a9da16dfb05e8
Thomas Maschler <tmaschler@wri.org>
2015-08-21 11:34:13 -0400

    update GAB resources

--- resources/Resources.js
+++ resources/Resources.js
@@ -19,7 +19,7 @@
 
         layersToShow: [],
 
-        maskMapUrl: "http://gis-forest-atlas.wri.org/arcgis/rest/services/GAB/GAB_00_africa/MapServer",
+        maskMapUrl: "http://gis.forest-atlas.org/arcgis/rest/services/GAB/GAB_00_africa/MapServer",
 
         // Not in Configuration Panel
         webMapID: "10db57b5316749478d5287155760fd14",
@@ -30,7 +30,7 @@
         // Not in Configuration Panel
         defaultLayerTransparency: 80,
         // Not in Configuration Panel
-        geometryServiceURL: "http://gis-forest-atlas.wri.org/arcgis/rest/services/Utilities/Geometry/GeometryServer",
+        geometryServiceURL: "http://gis.forest-atlas.org/arcgis/rest/services/Utilities/Geometry/GeometryServer",
 
         //flag
         countryFlagRight: "280px",
@@ -47,7 +47,7 @@
         downloadDataUrl: "http://gab-data.forest-atlas.org/",
 
         //print image
-        printURL: "http://gis-forest-atlas.wri.org/arcgis/rest/services/GAB/GABExportWebMap/GPServer/Export%20Web%20Map",
+        printURL: "http://gis.forest-atlas.org/arcgis/rest/services/GAB/GABExportWebMap/GPServer/Export%20Web%20Map",
 
         // Available Map Themes, These are completely controlled from the template, there are no default values
         // other then an empty array
