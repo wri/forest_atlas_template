05cbf89dc20eef47f63903369fa7d101ead562ce
Thomas Maschler <tmaschler@wri.org>
2015-08-27 17:38:05 -0400

    resource.js

--- resources/Resources.js
+++ resources/Resources.js
@@ -1,85 +1,95 @@
 ﻿define([], function() {
 
+    // TODO: This will later be broken into different sections based on what the user can configure in AGOL,
+    // for now, its in Alphabetical order because its easier to make sure we are not missing any keys that are in AGOL
+
     var o = {
 
-        //country code
-        country: "GAB",
+        aboutLinkUrl: 'http://www.wri.org/our-work/project/congo-basin-forests/gabon#project-tabs',
 
-        //languages
-        appLanguages: {
-            "en": {
-                title: "Forest Atlas of Gabon",
-                flagTitle: "Ministry of Forest, Environment and Protection of Natural Resources"
-            },
-            "fr": {
-                title: "Atlas Forestier du Gabon",
-                flagTitle: "Ministère de la Forêt, de l’Environnement et de la Protection des Ressources Naturelles"
-            }
-        },
+        country: 'GAB',
 
-        layersToShow: [],
+        countryFlagRight: 280,
 
-        maskMapUrl: "http://gis.forest-atlas.org/arcgis/rest/services/GAB/GAB_00_africa/MapServer",
+        countryTextWidth: 260,
 
-        // Not in Configuration Panel
-        webMapID: "10db57b5316749478d5287155760fd14",
+        defaultLanguage: 'en',
 
-        // Not in Configuration Panel
-        basemap: "national-geographic",
-        //graphics default transparency
-        // Not in Configuration Panel
-        defaultLayerTransparency: 80,
-        // Not in Configuration Panel
-        geometryServiceURL: "http://gis.forest-atlas.org/arcgis/rest/services/Utilities/Geometry/GeometryServer",
+        defaultTitle: 'Forest Atlas of Gabon',
 
-        //flag
-        countryFlagRight: "280px",
-        countryTextWidth: "260px",
-        flagPath: "app/images/GAB_flag_new.png",
-        flagLinkPath: "http://www.eaux-forets.gouv.ga/",
+        documentDirectory: 'https://wri-public-data.s3.amazonaws.com/forest_atlas/cmr/doc_pdf/',
 
-        //legislative text
-        pdfURL: "http://gab.forest-atlas.org/resources/docs/",
+        documentMapserver: 'http://54.88.79.102/arcgis/rest/services/CMR/documents_administratifs/MapServer',
 
-        aboutLinkUrl: "http://gab.forest-atlas.org",
+        downloadDataUrl: 'http://data.globalforestwatch.org/',
 
-        // Download Data URL
-        downloadDataUrl: "http://gab-data.forest-atlas.org/",
+        flagLinkPath: 'http://www.eaux-forets.gouv.ga/',
 
-        //print image
-        printURL: "http://gis.forest-atlas.org/arcgis/rest/services/GAB/GABExportWebMap/GPServer/Export%20Web%20Map",
+        flagPath: 'app/images/GAB_flag_new.png',
+
+        flagTitle: 'Ministry of Forest, Environment and Protection of Natural Resources',
+
+        layersToShow: [0, 1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 14, 15, 16, 17, 18, 19],
 
         // Available Map Themes, These are completely controlled from the template, there are no default values
         // other then an empty array
         mapThemes: [],
+
         mapThemesAlternate: [],
 
-        // toolsContainerFlag: "no_ministry_logo_sm.png",
+        maskMapUrl: 'http://gis-forest-atlas.wri.org/arcgis/rest/services/GAB/GAB_00_africa/MapServer',
 
-        // toolsFlagContainerLink: "http://www.regjeringen.no/en/dep/kld.html?id=668",
+        printURL: 'http://gis-forestatlas.wri.org/arcgis/rest/services/GAB/GABExportWebMap/GPServer/Export%20Web%20Map',
 
-        // true means this analysis will be performed, false means it will not be performed
-        // analysisSets: {
-        //     ifl: true,
-        //     carbon: true,
-        //     landCover: true,
-        //     treeDensity: true,
-        //     protectedArea: true,
-        //     primForest: true,
-        //     legal: true,
-        //     peat: true,
-        //     rspo: true
-        // },
+        secondLanguage: 'fr',
 
-        // analysisCategories: {
-        //     // These may not need to be in here depending on how much cusotmmizaiton
-        //     // we want each application to have
-        //     hansen: true, // tree cover loss
-        //     forma: true, // clearance alerts
-        //     fires: true
-        // }
+        secondLanguageTitle: 'Atlas Forestier du Gabon',
+
+        secondLanguageFlagTitle: 'Ministère de la Forêt, de l’Environnement et de la Protection des Ressources Naturelles',
+
+        useAdditionalLanguage: true,
+
+        webMapID: '10db57b5316749478d5287155760fd14',
+
+        // NOW FOR THE LAYER SETTINGS
+        // These are placeholders, they currently do nothing but they will soon control which layers/analysis types are visible
+        activeFiresIncluded: true,
+
+        landCoverIncluded: true,
+
+        iflIncluded: true,
+
+        biomassIncluded: true,
+
+        // NOT PRESENT IN THE APPLICATION YET
+        formaIncluded: false,
+
+        terraIncluded: false,
+
+        imazonIncluded: false,
+
+        globcoverIncluded: false,
+
+        mangroveIncluded: false
 
     };
+
+    // This should NEVER be directly edited, edit the properties above, this object is usually created in code at runtime
+    // but if there is no appid and the app cannot read info from AGOL, it will need to use this as a default for
+    // the usable language(s)
+    o.appLanguages = {};
+
+    o.appLanguages[o.defaultLanguage] = {
+      title: o.defaultTitle,
+      flagTitle: o.flagTitle
+    };
+
+    if (o.useAdditionalLanguage) {
+      o.appLanguages[o.secondLanguage] = {
+        title: o.secondLanguageTitle,
+        flagTitle: o.secondLanguageFlagTitle
+      };
+    }
 
     return o;
 });
