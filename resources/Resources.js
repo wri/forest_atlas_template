a28174172067fb287f638e8eabebd6e53c5e33fd
Thomas Maschler <tmaschler@wri.org>
2015-08-28 13:55:10 -0400

    resources.js

--- resources/Resources.js
+++ resources/Resources.js
@@ -29,7 +29,7 @@
 
         flagTitle: 'Ministry of Forest, Environment and Protection of Natural Resources',
 
-        layersToShow: [0, 1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 14, 15, 16, 17, 18, 19],
+        layersToShow: [0, 1, 2, 3, 4, 9, 10, 11, 14, 15, 16],
 
         // Available Map Themes, These are completely controlled from the template, there are no default values
         // other then an empty array
@@ -49,7 +49,7 @@
 
         useAdditionalLanguage: true,
 
-        webMapID: '10db57b5316749478d5287155760fd14',
+        webMapID: '7903f477738b4fa7827da77cf19190f5',
 
         // NOW FOR THE LAYER SETTINGS
         // These are placeholders, they currently do nothing but they will soon control which layers/analysis types are visible
