ad1af9373a39335785bd2732afb7d06cbc99f128
Thomas Maschler <tmaschler@wri.org>
2015-09-01 18:53:27 -0400

    resources.js

--- resources/Resources.js
+++ resources/Resources.js
@@ -1,35 +1,36 @@
-﻿define([], function() {
+﻿define([], function () {
+  "use strict";
 
     // TODO: This will later be broken into different sections based on what the user can configure in AGOL,
     // for now, its in Alphabetical order because its easier to make sure we are not missing any keys that are in AGOL
 
     var o = {
 
-        aboutLinkUrl: 'http://cog.forest-atlas.org/',
+        aboutLinkUrl: "http://gab.forest-atlas.org/",
 
-        country: 'COG',
+        country: "GAB",
 
         countryFlagRight: 280,
 
         countryTextWidth: 260,
 
-        defaultLanguage: 'en',
+        defaultLanguage: "en",
 
-        defaultTitle: 'Forest Atlas of Gabon',
+        defaultTitle: "Forest Atlas of Gabon",
 
-        documentDirectory: 'http://cog.data.wri.org/forest_atlas/resources/docs/',
+        documentDirectory: "http://gab.forest-atlas.org/resources/docs/",
 
-        documentMapserver: 'http://gis.forest-atlas.org/arcgis/rest/services/COG/documents_administratifs/MapServer',
+        documentMapserver: "http://gis.forest-atlas.org/arcgis/rest/services/GAB/documents_administratifs/MapServer",
 
-        downloadDataUrl: 'http://cog-data.forest-atlas.org/',
+        downloadDataUrl: "http://gab-data.forest-atlas.org/",
 
-        flagLinkPath: 'http://www.mefdd.cg/',
+        flagLinkPath: "http://www.eaux-forets.gouv.ga/",
 
-        flagPath: 'app/images/COG_flag_new.png',
+        flagPath: "app/images/GAB_flag_new.png",
 
-        flagTitle: 'Forest Atlas of Republic of Congo',
+        flagTitle: "Ministry of Forest, Environment and Protection of Natural Resources",
 
-        layersToShow: [0, 1, 2, 3, 4, 6, 7, 8, 9, 10, 11, 14, 15, 16, 17, 18, 19],
+        layersToHide: [5,6,7,8,12,13],
 
         // Available Map Themes, These are completely controlled from the template, there are no default values
         // other then an empty array
@@ -37,19 +38,19 @@
 
         mapThemesAlternate: [],
 
-        maskMapUrl: 'http://gis.forest-atlas.org/arcgis/rest/services/COG/COG_00_africa/MapServer',
+        maskMapUrl: "http://gis.forest-atlas.org/arcgis/rest/services/GAB/GAB_00_africa/MapServer",
 
-        printURL: 'http://gis.forest-atlas.org/arcgis/rest/services/COG/COGExportWebMap/GPServer/Export%20Web%20Map',
+        printURL: "http://gis.forest-atlas.org/arcgis/rest/services/GAB/GABExportWebMap/GPServer/Export%20Web%20Map",
 
-        secondLanguage: 'fr',
+        secondLanguage: "fr",
 
-        secondLanguageTitle: 'Atlas Forestier de la République du Congo',
+        secondLanguageTitle: "Atlas Forestier du Gabon",
 
-        secondLanguageFlagTitle: 'Ministère de l’Économie Forestière et du Développement Durable',
+        secondLanguageFlagTitle: "Ministère de la Forêt, de l’Environnement et de la Protection des Ressources Naturelles",
 
         useAdditionalLanguage: true,
 
-        webMapID: 'abb18c1bfdd74cc9982d6535d2451692',
+        webMapID: "10db57b5316749478d5287155760fd14",
 
         // NOW FOR THE LAYER SETTINGS
         // These are placeholders, they currently do nothing but they will soon control which layers/analysis types are visible
