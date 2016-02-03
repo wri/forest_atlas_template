aa45924fb6ad912a64c11ce9b620a84599b17130
Robert Winterbottom <rwinterbottom@blueraster.com>
2016-02-03 09:26:34 -0500

    Prepared new test build

--- www/app/js/analysis/constants.js
+++ www/app/js/analysis/constants.js
@@ -1,1 +1,1 @@
-define({TREE_COVER:"tree_cover",LAND_COVER:"land_cover",POPULATION:"population",SLOPE:"slope",TREE_COVER_CHART_ID:"tree-cover-chart",LAND_COVER_CHART_ID:"land-cover-chart",POPULATION_CHART_ID:"population-chart",SLOPE_CHART_ID:"slope-chart"});
+define({TREE_COVER:"tree_cover",LAND_COVER:"land_cover",POPULATION:"population",SLOPE:"slope",SLOPE_BREAKDOWN:"slope_breakdown",TREE_COVER_CHART_ID:"tree-cover-chart",LAND_COVER_CHART_ID:"land-cover-chart",POPULATION_CHART_ID:"population-chart",SLOPE_CHART_ID:"slope-chart",SLOPE_BREAKDOWN_CHART_ID:"analysis-chart"});
