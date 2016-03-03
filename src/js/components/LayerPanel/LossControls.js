3e661045db4addb86550c35c46f6504c3a790cbd
swingley <swingley@gmail.com>
2016-03-03 13:54:13 -0800

    Use component state to manage what's shown in the loss controls drop-downs.

--- src/js/components/LayerPanel/LossControls.js
+++ src/js/components/LayerPanel/LossControls.js
@@ -1,17 +1,16 @@
-import {layerActions} from 'actions/LayerActions';
+import layerActions from 'actions/LayerActions';
 import layerUtils from 'js/utils/layerUtils';
 import LayersHelper from 'helpers/LayersHelper';
 import {layerPanelText} from 'js/config';
 import React from 'react';
 
-// let lossOptions = layerPanelText.lossOptions;
+let lossOptions = [];
 
 export default class LossControls extends React.Component {
   constructor (props) {
     super(props);
     this.state = {
       loaded: false,
-      lossOptions: [],
       lossFromSelectIndex: null,
       lossToSelectIndex: null
     };
@@ -21,7 +20,6 @@ export default class LossControls extends React.Component {
     // TODO:  pull from config.
     let url = 'http://gis-treecover.wri.org/arcgis/rest/services/ForestCover_lossyear/ImageServer';
     layerUtils.getLayerMetadata(url).then((results) => {
-      let lossOptions = [];
       let min = results.minValues[0];
       let max = results.maxValues[0];
       for ( let i = min; i <= max; i++ ) {
@@ -29,7 +27,6 @@ export default class LossControls extends React.Component {
       }
       this.setState({
         loaded: true,
-        lossOptions: lossOptions,
         lossFromSelectIndex: 0,
         lossToSelectIndex: lossOptions.length-1
       });
@@ -37,34 +34,33 @@ export default class LossControls extends React.Component {
   }
 
   componentDidUpdate (prevProps) {
+    console.log('componentDidUpdate', prevProps, this.props);
+    // TODO:  call the layer helper here to update layer def / raster func
+    // with new info from state, not props
     if (prevProps.lossFromSelectIndex !== this.props.lossFromSelectIndex || prevProps.lossToSelectIndex !== this.props.lossToSelectIndex) {
       LayersHelper.updateLossLayerDefinitions(this.props.lossFromSelectIndex, this.props.lossToSelectIndex);
     }
   }
 
-  componentWillReceiveProps(nextProps) {
-    // Set the default layer definition when the map has been loaded
-    if (!this.props.loaded && nextProps.loaded) {
-      LayersHelper.updateLossLayerDefinitions(this.props.lossFromSelectIndex, this.props.lossToSelectIndex);
-    }
-  }
-
   renderSelects () {
+    console.log('loss controls render', this.props)
     let selects = <div className='timeline-container loss flex'>loading...</div>;
-    if ( this.state.loaded ) {
-      let fromItem = this.state.lossOptions[this.state.lossFromSelectIndex];
-      let toItem = this.state.lossOptions[this.state.lossToSelectIndex];
+    if ( this.props.loaded ) {
+      let fromItem = lossOptions[this.state.lossFromSelectIndex];
+      let toItem = lossOptions[this.state.lossToSelectIndex];
+      // let fromItem = lossOptions[this.props.lossFromSelectIndex];
+      // let toItem = lossOptions[this.props.lossToSelectIndex];
       selects = <div className='timeline-container loss flex'>
         <div className='loss-from relative'>
-          <select onChange={this.fromChanged} className='pointer' value={fromItem.value}>
-            {this.state.lossOptions.map(this.optionsMap('from'))}
+          <select onChange={this.fromChanged.bind(this)} className='pointer' value={fromItem.value}>
+            {lossOptions.map(this.optionsMap('from'))}
           </select>
           <div className='loss-from-button fa-button sml white'>{fromItem.label}</div>
         </div>
         <div className='loss-timeline-spacer'>to</div>
         <div className='loss-to relative'>
-          <select onChange={this.toChanged} className='pointer' value={toItem.value}>
-            {this.state.lossOptions.map(this.optionsMap('to'))}
+          <select onChange={this.toChanged.bind(this)} className='pointer' value={toItem.value}>
+            {lossOptions.map(this.optionsMap('to'))}
           </select>
           <div className='loss-to-button fa-button sml white'>{toItem.label}</div>
         </div>
@@ -82,8 +78,8 @@ export default class LossControls extends React.Component {
   optionsMap (selectType) {
     // Disable options in the 'from' select that are greater than the selected value in the 'to' select
     // and vice versa, disable 'to' options less than the selected value in the 'from' select
-    let fromMax = this.state.lossOptions[this.state.lossToSelectIndex].value;
-    let toMin = this.state.lossOptions[this.state.lossFromSelectIndex].value;
+    let fromMax = lossOptions[this.state.lossToSelectIndex].value;
+    let toMin = lossOptions[this.state.lossFromSelectIndex].value;
     return (item, index) => {
       let disabled = selectType === 'from' ? item.value >= fromMax : item.value <= toMin;
       return <option key={index} value={item.value} disabled={disabled}>{item.label}</option>;
@@ -91,11 +87,25 @@ export default class LossControls extends React.Component {
   }
 
   fromChanged (evt) {
-    layerActions.changeLossFromTimeline(evt.target.selectedIndex);
+    if (evt.target.selectedIndex !== this.state.lossFromSelectIndex) {
+      this.setState({
+        loaded: true,
+        lossFromSelectIndex: evt.target.selectedIndex,
+        lossToSelectIndex: this.state.lossToSelectIndex
+      });
+      layerActions.changeLossFromTimeline(evt.target.selectedIndex);
+    }
   }
 
   toChanged (evt) {
-    layerActions.changeLossToTimeline(evt.target.selectedIndex);
+    if (evt.target.selectedIndex !== this.state.lossToSelectIndex) {
+      this.setState({
+        loaded: true,
+        lossFromSelectIndex: this.state.lossFromSelectIndex,
+        lossToSelectIndex: evt.target.selectedIndex
+      });
+      layerActions.changeLossToTimeline(evt.target.selectedIndex);
+    }
   }
 
 }
